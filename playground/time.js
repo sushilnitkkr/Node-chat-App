12c655e4b6e1ab55a0e699803ce99186a7395c75
sushil kumar <sushil@DESKTOP>
2019-02-07 14:48:56 +0530

    Add mustache.js for message template

--- playground/time.js
+++ playground/time.js
@@ -4,6 +4,8 @@ const moment = require('moment');
 // var date = new Date();
 // var months = ['Jan','Feb']
 // console.log(date);
-var date = moment();
-console.log(date.format(' Do MMM YYYY'));
-console.log(date.format('h:mm a'));
+// var date = moment();
+// console.log(date.format(' Do MMM YYYY'));
+var createdAt = 1234;
+var date moment(createdAt);
+ console.log(date.format('h:mm a'));
